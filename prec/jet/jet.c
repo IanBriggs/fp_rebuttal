3cadc735572049a5f3fa143256f9b0e12fe99a47
Ian Briggs <ianbriggsutah@gmail.com>
2016-08-22 19:03:43 +0000

    Broke up all operations into new values so that prec can do the same problem we do

--- prec/jet/jet.c
+++ prec/jet/jet.c
@@ -10,7 +10,7 @@
 #include <unistd.h>
 
 extern uint64_t current_time_ns(void);
-const size_t INPUTS = 10;
+const size_t INPUTS = 1000;
 
 double jet(double x1, double x2)
 {
@@ -24,11 +24,33 @@ double jet(double x1, double x2)
   double __const_2 = 1.0;
   double __const_1 = 2.0;
   double __const_0 = 3.0;
-
-  
-  double ____expr_0 = (x1 + (((((((((__const_3 * x1) * ((((__const_0 * (x1 * x1)) + (__const_1 * x2)) - x1) / ((x1 * x1) + __const_2))) * (((((__const_0 * (x1 * x1)) + (__const_1 * x2)) - x1) / ((x1 * x1) + __const_2)) - __const_4)) + ((x1 * x1) * ((__const_5 * ((((__const_0 * (x1 * x1)) + (__const_1 * x2)) - x1) / ((x1 * x1) + __const_2))) - __const_6))) * ((x1 * x1) + __const_7)) + ((__const_8 * (x1 * x1)) * ((((__const_0 * (x1 * x1)) + (__const_1 * x2)) - x1) / ((x1 * x1) + __const_2)))) + ((x1 * x1) * x1)) + x1) + (__const_9 * ((((__const_0 * (x1 * x1)) + (__const_1 * x2)) - x1) / ((x1 * x1) + __const_2)))));
-
-  return ____expr_0;
+  double _expr_0   = __const_3 * x1;        // ['*', ['Variable', '__const_3'], ['Input', 'x1']]
+  double _expr_1   = x1        * x1;        // ['*', ['Input', 'x1'], ['Input', 'x1']]
+  double _expr_2   = __const_0 * _expr_1;   // ['*', ['Variable', '__const_0'], ['Variable', '_expr_1']]
+  double _expr_3   = __const_1 * x2;        // ['*', ['Variable', '__const_1'], ['Input', 'x2']]
+  double _expr_4   = _expr_2   + _expr_3;   // ['+', ['Variable', '_expr_2'], ['Variable', '_expr_3']]
+  double _expr_5   = _expr_4   - x1;        // ['-', ['Variable', '_expr_4'], ['Input', 'x1']]
+  double _expr_6   = _expr_1   + __const_2; // ['+', ['Variable', '_expr_1'], ['Variable', '__const_2']]
+  double _expr_7   = _expr_5   / _expr_6;   // ['/', ['Variable', '_expr_5'], ['Variable', '_expr_6']]
+  double _expr_8   = _expr_0   * _expr_7;   // ['*', ['Variable', '_expr_0'], ['Variable', '_expr_7']]
+  double _expr_9   = _expr_7   - __const_4; // ['-', ['Variable', '_expr_7'], ['Variable', '__const_4']]
+  double _expr_10  = _expr_8   * _expr_9;   // ['*', ['Variable', '_expr_8'], ['Variable', '_expr_9']]
+  double _expr_11  = __const_5 * _expr_7;   // ['*', ['Variable', '__const_5'], ['Variable', '_expr_7']]
+  double _expr_12  = _expr_11  - __const_6; // ['-', ['Variable', '_expr_11'], ['Variable', '__const_6']]
+  double _expr_13  = _expr_1   * _expr_12;  // ['*', ['Variable', '_expr_1'], ['Variable', '_expr_12']]
+  double _expr_14  = _expr_10  + _expr_13;  // ['+', ['Variable', '_expr_10'], ['Variable', '_expr_13']]
+  double _expr_15  = _expr_1   + __const_7; // ['+', ['Variable', '_expr_1'], ['Variable', '__const_7']]
+  double _expr_16  = _expr_14  * _expr_15;  // ['*', ['Variable', '_expr_14'], ['Variable', '_expr_15']]
+  double _expr_17  = __const_8 * _expr_1;   // ['*', ['Variable', '__const_8'], ['Variable', '_expr_1']]
+  double _expr_18  = _expr_17  * _expr_7;   // ['*', ['Variable', '_expr_17'], ['Variable', '_expr_7']]
+  double _expr_19  = _expr_16  + _expr_18;  // ['+', ['Variable', '_expr_16'], ['Variable', '_expr_18']]
+  double _expr_20  = _expr_1   * x1;        // ['*', ['Variable', '_expr_1'], ['Input', 'x1']]
+  double _expr_21  = _expr_19  + _expr_20;  // ['+', ['Variable', '_expr_19'], ['Variable', '_expr_20']]
+  double _expr_22  = _expr_21  + x1;        // ['+', ['Variable', '_expr_21'], ['Input', 'x1']]
+  double _expr_23  = __const_9 * _expr_7;   // ['*', ['Variable', '__const_9'], ['Variable', '_expr_7']]
+  double _expr_24  = _expr_22  + _expr_23;  // ['+', ['Variable', '_expr_22'], ['Variable', '_expr_23']]
+  double _expr_25  = x1        + _expr_24;  // ['+', ['Input', 'x1'], ['Variable', '_expr_24']]
+  return _expr_25;
 }
 
 int main() {
