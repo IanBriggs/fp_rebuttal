458cc294a07f6c9e3673989f5224c0a18ab3ba79
Ian Briggs <ianbriggsutah@gmail.com>
2016-08-22 16:59:43 +0000

    First commit

---
+++ prec/jet/jet.c
@@ -0,0 +1,100 @@
+#include "logging/cov_log.h"
+#include "logging/cov_checker.h"
+#include "logging/cov_serializer.h"
+
+#include <time.h>
+#include <inttypes.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+double jet(double x1, double x2)
+{
+  double __const_4 = 3.0;
+  double __const_7 = 1.0;
+  double __const_9 = 3.0;
+  double __const_8 = 3.0;
+  double __const_6 = 6.0;
+  double __const_5 = 4.0;
+  double __const_3 = 2.0;
+  double __const_2 = 1.0;
+  double __const_1 = 2.0;
+  double __const_0 = 3.0;
+
+  
+  double ____expr_0 = (x1 + (((((((((__const_3 * x1) * ((((__const_0 * (x1 * x1)) + (__const_1 * x2)) - x1) / ((x1 * x1) + __const_2))) * (((((__const_0 * (x1 * x1)) + (__const_1 * x2)) - x1) / ((x1 * x1) + __const_2)) - __const_4)) + ((x1 * x1) * ((__const_5 * ((((__const_0 * (x1 * x1)) + (__const_1 * x2)) - x1) / ((x1 * x1) + __const_2))) - __const_6))) * ((x1 * x1) + __const_7)) + ((__const_8 * (x1 * x1)) * ((((__const_0 * (x1 * x1)) + (__const_1 * x2)) - x1) / ((x1 * x1) + __const_2)))) + ((x1 * x1) * x1)) + x1) + (__const_9 * ((((__const_0 * (x1 * x1)) + (__const_1 * x2)) - x1) / ((x1 * x1) + __const_2)))));
+
+  return ____expr_0;
+}
+
+int main() {
+  // variables for logging/checking
+  long double log[INPUTS];
+  long double threshold = 5e-3; 
+
+  // 0. read input from the file final_inputs
+  int finputs1[INPUTS];
+
+  FILE* infile1 = fopen("final_inputs1", "r");
+  if (!infile1) 
+  {
+    printf("Could not open final_inputs1\n");
+  }
+
+  char *s = malloc(10);
+  for (i = 0; i < INPUTS; i++)
+  {
+    if (!feof(infile1))
+    {
+      fscanf(infile1, "%s", s);
+      finputs1[i] = (int)cov_deserialize(s, 10);
+    }
+  }
+
+  int finputs2[INPUTS];
+
+  FILE* infile2 = fopen("final_inputs2", "r");
+  if (!infile2) 
+  {
+    printf("Could not open final_inputs2\n");
+  }
+
+  char *s = malloc(10);
+  for (i = 0; i < INPUTS; i++)
+  {
+    if (!feof(infile2))
+    {
+      fscanf(infile2, "%s", s);
+      finputs2[i] = (int)cov_deserialize(s, 10);
+    }
+  }
+
+
+
+  start = current_time_ns();
+  for (l = 0; l < INPUTS; l++)
+  {
+    double x1 = finputs1[l];
+    double x2 = finputs2[l];
+    log[l] = jet(x1, x2);
+  }
+  end = current_time_ns();
+
+  diff = (end-start);
+
+
+  // 2. create spec, or checking results
+  cov_arr_spec_log("spec.cov", threshold, INPUTS, log);
+  cov_arr_log(log, INPUTS, "result", "log.cov");
+  cov_check("log.cov", "spec.cov", INPUTS);
+
+  // 3. print score (diff) to a file
+  FILE* file;
+  file = fopen("score.cov", "w");
+  fprintf(file, "%ld\n", diff);
+  fclose(file);
+
+  return 0;
+}
---
+++ prec/jet/jet_input.c
@@ -0,0 +1,119 @@
+#include "logging/cov_log.h"
+#include "logging/cov_serializer.h"
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <math.h>
+#include <time.h>
+
+const size_t INPUTS = 10;
+
+uint64_t
+rand_32_bit()
+{
+  static size_t bit_index = 31;
+  static uint64_t rand_bitfield;
+
+  if (bit_index == 31) {
+    // random() returns a signed, positive 32 bit value
+    // this means the highest bit will always be 0
+    // to get a 32 bit random value we take random bits
+    // from another call to random and put them in the highest
+    // bit position. Every 31 calls we run out of random bits
+    // and must do an extra call to random()
+    rand_bitfield = (uint64_t) random();
+    bit_index = 0;
+  }
+
+  uint64_t r = (uint64_t) random();
+  r |= ((rand_bitfield >> bit_index) & 0x1) << 31;
+  bit_index++;
+
+  return r;
+}
+
+
+uint64_t
+rand_uint64_t(void)
+{
+  return rand_32_bit() | (rand_32_bit() << 32);
+}
+
+
+double
+rand_real_range(const double low, const double high)
+{
+  assert(!isnan(low));
+  assert(!isnan(high));
+  assert(low <= high);
+
+  const double r = ((double) rand_uint64_t()) / (double) UINT64_MAX;
+  const double diff = high - low;
+  const double rr = r * diff;
+  const double retval = low + rr;
+
+  assert(retval >= low);
+  assert(retval <= high);
+  return retval;
+}
+
+
+int 
+main()
+{
+  srandom(time(NULL));
+  double inputs1[INPUTS];
+  double inputs2[INPUTS];
+
+  for (size_t i=0; i<INPUTS; i++) {
+    inputs1[i] = rand_real_range(-5.0, 5.0);
+    inputs2[i] = rand_real_range(-20.0, 5.0);
+    cov_log("", "final_inputs1", 1, inputs1[i]);
+    cov_log("", "final_inputs2", 1, inputs2[i]);
+  }
+
+
+
+
+
+  double finputs1[INPUTS];
+
+  FILE* infile1 = fopen("final_inputs1", "r");
+  if (!infile1) 
+  {
+    printf("Could not open final_inputs1\n");
+  }
+
+  char *s = malloc(sizeof(long double));
+  for (size_t i = 0; i < INPUTS; i++)
+  {
+    if (!feof(infile1))
+    {
+      fscanf(infile1, "%s", s);
+      finputs1[i] = cov_deserialize(s, sizeof(long double));
+    }
+    printf("%f ==? %f \n", inputs1[i], finputs1[i]);
+  }
+
+  double finputs2[INPUTS];
+
+  FILE* infile2 = fopen("final_inputs2", "r");
+  if (!infile2) 
+  {
+    printf("Could not open final_inputs2\n");
+  }
+
+  //char *s = malloc(10);
+  for (size_t i = 0; i < INPUTS; i++)
+  {
+    if (!feof(infile2))
+    {
+      fscanf(infile2, "%s", s);
+      finputs2[i] = cov_deserialize(s, sizeof(long double));
+    }
+    printf("%f ==? %f \n", inputs2[i], finputs2[i]);
+  }
+
+}
